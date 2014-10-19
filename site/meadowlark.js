80e47cc978888c1530bd371fdcd411c4994ecab6
cyper <345343747@qq.com>
2014-10-19 23:28:10 +0800

    chapter3.

---
+++ site/meadowlark.js
@@ -0,0 +1,44 @@
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
+
+var express = require('express');
+var app = express();
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/about',function(req,res){
+    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about',{fortune: randomFortune});
+});
+
+// custom 404 page
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+app.use(function(err,req,res,next){
+    console.error(err.statck);
+    res.status(500);
+    res.render('500');
+
+});
+
+app.listen(app.get('port'),function(){
+    console.log('Express started on http://localhost:' +
+        app.get('port') +'; press Ctrl-C to terminate.'
+    );
+});
